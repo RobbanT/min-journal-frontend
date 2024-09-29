c1c1d10890900ad837fa1bd9900346de1dc1b3d7
Roberto Tencic <roberto.tencic@outlook.com>
2024-09-29 22:53:24 +0200

    Mer CSS.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    NavComponent,
+    MainComponent,
+    LoginComponent,
+    RegisterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
+import { RegisterComponent } from '../register/register.component';
+import { LoginComponent } from '../login/login.component';
+import { JournalComponent } from '../journal/journal.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [HomeComponent, RegisterComponent, LoginComponent, JournalComponent],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrl: './main.component.css',
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
